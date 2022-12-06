054b536aeef990b3bb5504441cee381519c6cc5b
BrandonBishop84 <brandonnovember@gmail.com>
2022-12-06 09:56:39 -0800

    final version

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -10,7 +10,7 @@ const Results = ({ result }) => {
         <div className='container'>
             <ul className='resultscard'>
                 <li className='listitem'>
-                    <h1 className='title'>Title: {result.title}</h1>
+                    <h1 className='title'>{result.title}</h1>
                     <img className='image' src={`${result.thumbnail.path}.${result.thumbnail.extension}`}/> 
                     {/* <h2 className='description'>Description: {result.description}</h2> */}
                 </li>
